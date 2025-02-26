398b18030767dccb197eeb6a3c527ffab6cf5f98
Kyle Humber <kwhumber@gmail.com>
2025-02-26 00:08:58 -0700

    Biggest add is that now the game checks the absolute speed in any direction to determine the sprite to use. Added Bullet Sprite and begun thinking about bullet logic
    Don't like the flying behaviour and how keys are interacting with each other but, it does seem better than before.

--- asteroid.js
+++ asteroid.js
@@ -2,7 +2,8 @@
 
 let angleConstant = 12;
 let halfShip = 24;
-let drift = 0.989;
+//let bulletDist = 15;
+let drift = 0.993;
 
 //get our canvas then target it's context
 const c = document.getElementById("game-board");
@@ -24,54 +25,81 @@ shipExhausting.src = "./Sprites/Player/ship_release_48.png";
 const shipSteady = new Image();
 shipSteady.src = "./Sprites/Player/ship_48.png";  
 
+const bullet = new Image();
+bullet.src = "./Sprites/Player/bullet.png";  
 
 
-//Define the player class
-/* code snipet I wanted to save
-                    ctx.save()
-                    ctx.translate(this.position.x, this.position.y);
-                    ctx.rotate(Math.PI / 2);  //change this to evaluate dynamically later
-                    ctx.drawImage(ship_3, -ship_3.width / 2, -ship_3.height / 2);                   
-                    ctx.restore();
-*/
 
 class Player {
     constructor() {
         this.position = {
-            x : c.width / 2 - halfShip,
-            y : c.height / 2 - halfShip       
+            x : c.width / 2,
+            y : c.height / 2
         }
         this.velocity = {
             x : 0,
             y: 0,
         }
-        this.stationary = true;        }
+        this.stationary = true;
+        this.drifting = false;
+    }
            
     draw() {
 
+        //check ship against canvas boundaries before drawing and reposition to opposite side if needed
+        if (this.position.x - halfShip > c.width) {
+            this.position.x = 0 - halfShip;
+        }
+
+        if (this.position.x + halfShip < 0) {
+            this.position.x = c.width + halfShip;
+        }
+
+        if (this.position.y - halfShip > c.height) {
+            this.position.y = 0 - halfShip;
+        }
+
+        if (this.position.y + halfShip < 0) {
+                this.position.y = c.height + halfShip;
+        }
+
+        // rotate and draw the ship based on angleConstant
+        ctx.save()
+        ctx.shadowColor = "black";
+        ctx.shadowOffsetX = 15;
+        ctx.shadowOffsetY = 25;
+        ctx.shadowBlur = 13;
+        ctx.translate(this.position.x, this.position.y);
+        ctx.rotate(angleConstant * Math.PI / 6);
+        ctx.translate(-halfShip, -halfShip);
+
+        //choose ship based on speed state
         if (this.stationary) {
-            ctx.save()
-            ctx.translate(this.position.x, this.position.y);
-            ctx.rotate(angleConstant * Math.PI / 6);
-            console.log("Draw Method :", angleConstant);
-            ctx.translate(-halfShip, -halfShip);
             ctx.drawImage(shipSteady, 0, 0);
-            ctx.restore();
+        } else if (this.drifting) {
+            ctx.drawImage(shipExhausting, 0, 0);
         } else {
-            ctx.save()
-            ctx.translate(c.width / 2, c.height / 2);
-            ctx.rotate(angleConstant * Math.PI / 6);
-            console.log("Draw Method :", angleConstant);
-            ctx.translate(-halfShip, -halfShip);
             ctx.drawImage(shipPower, 0, 0);
-            ctx.restore();
-        }  
+        }
+
+        ctx.restore();
     }
+
+
     update() {
         this.draw();
         this.position.x -= this.velocity.x;
         this.position.y += this.velocity.y;
     }
+
+    //fire() {
+    //    ctx.save();
+    //    ctx.translate(this.position.x - halfShip, this.position.y + halfShip);
+    //    ctx.rotate(angleConstant * Math.PI / 6);
+    //    ctx.translate(-8, -8);
+    //    ctx.drawImage(bullet, 0, 0);
+    //    ctx.restore();
+    //}
 }
 
 const hero = new Player();
@@ -84,6 +112,17 @@ const animate = () => {
     hero.update();
     hero.velocity.y = hero.velocity.y * drift;
     hero.velocity.x = hero.velocity.x * drift;
+    if (Math.abs(hero.velocity.y) + Math.abs(hero.velocity.x) > 0.75) {
+        hero.stationary = false;
+        hero.drifting = false;
+    } else if (Math.abs(hero.velocity.y) + Math.abs(hero.velocity.x) <= 0.75 && Math.abs(hero.velocity.y) + Math.abs(hero.velocity.x) > 0.4) {
+        hero.stationary = false;
+        hero.drifting = true;
+    } else {
+        hero.stationary = true;
+        hero.drifting = false;
+    }
+
 }
 
 //size the game based on the size available on loading.
@@ -92,46 +131,40 @@ window.onload = (event) => {
 };
 
 
-const movePlayer = (key, pressed) => {
-    switch (key) {
-        case "ArrowUp":
-            if (pressed) {
-                hero.velocity.y = hero.position.y - (hero.position.y + 2 * Math.cos(angleConstant * (Math.PI / 6)));
-                hero.velocity.x = hero.position.x - (hero.position.x + 2 * Math.sin(angleConstant * (Math.PI / 6)));                
-            }
-            console.log("ArrowUp: ", angleConstant);
-            break;
+const movePlayer = (keys) => {
 
-        case "ArrowLeft":
-            if (pressed) {
-                if (angleConstant === 12) {
-                    angleConstant = 1;
-                } else {
-                    angleConstant++;
-                }
-            }
-            console.log("ArrowLeft: ", angleConstant);
-            break;
+    if (keys["ArrowUp"] === true) {
+            hero.velocity.y = hero.position.y - (hero.position.y + 1.75 * Math.cos(angleConstant * (Math.PI / 6)));
+            hero.velocity.x = hero.position.x - (hero.position.x + 1.75 * Math.sin(angleConstant * (Math.PI / 6)));
+        }
 
-        case "ArrowRight":
-            if (pressed) {
-                if (angleConstant === 1) {
-                    angleConstant = 12;
-                } else {
-                    angleConstant--;
-                }
-            }
-            console.log("ArrowRight: ", angleConstant);
-            break;
+
+    if (keys["ArrowRight"] === true) {
+        if (angleConstant === 12) {
+            angleConstant = 1;
+        } else {
+            angleConstant++;
+        }
+    }
+
+
+    if (keys["ArrowLeft"] === true) {
+        if (angleConstant === 1) {
+            angleConstant = 12;
+        } else {
+            angleConstant--;
+        }
     }
 
 }
+
+
 
 /* PI ROTATION RATIOS FOR LATER !!!javascript rotation happens in clockwise direction!!!
 0 degrees = 12 * PI / 6
 30 degrees = 1 * PI / 6
 60 degrees = 2 * PI / 6
-90 degrees = 3 * PI / 6  // this turned the ship to face the left border
+90 degrees = 3 * PI / 6 
 120 degrees = 4 * PI / 6
 150 degrees = 5 * PI / 6
 180 degrees = 6 * PI / 6
@@ -142,16 +175,21 @@ const movePlayer = (key, pressed) => {
 330 degrees = 11 * PI / 6
 360 degrees = 12 * PI / 6
 
-//thinking of using the constant to feed in the rotation of the ship
-let rotationConst = 0; default is spawn facing top of page
 */
 
-window.addEventListener("keydown", ({key}) => {
-    movePlayer(key, true);
+let keysPressed = {};
+window.addEventListener("keydown", ({ key }) => {
+    //if (key === " ") {
+    //    hero.fire();
+    //    console.log("fire");
+    //}
+    keysPressed[key] = true; 
+    movePlayer(keysPressed);
 });
 
 
 window.addEventListener("keyup", ({ key }) => {
-    movePlayer(key, false);
+    keysPressed[key] = false;
+    movePlayer(keysPressed);
 });
 
